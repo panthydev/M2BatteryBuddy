1622e7712b18a2f89a4ed0b57ff40f487d36844b
panthydev <panthydev@gmail.com>
2026-05-07 13:39:19 +0200

    more javadocs. im over here doccing my java

--- app/src/main/java/com/panthydev/m2batteryapp/Interfaces/Callback.java
+++ app/src/main/java/com/panthydev/m2batteryapp/Interfaces/Callback.java
@@ -2,8 +2,9 @@ package com.panthydev.m2batteryapp.Interfaces;
 
 public interface Callback<T>
 {
-
+    /**
+     *good luck.
+     * @param Result The result of the DB search, T is of type DataPack<T> where T (inner) is a subclass of DataObject.
+     */
     void OnResult(T Result);
-
-
 }
