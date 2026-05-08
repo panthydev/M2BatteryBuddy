0af68bad3b2f7c31c79d942e026b9e5ad6fd7554
MalteSL-Git <maltesoendergaardludvigsen@gmail.com>
2026-05-08 15:39:10 +0200

    Made the AccordionMenu unfold with both the button and the cardview itself

--- app/src/main/java/com/panthydev/m2batteryapp/AccordionMenu.java
+++ app/src/main/java/com/panthydev/m2batteryapp/AccordionMenu.java
@@ -25,6 +25,29 @@ public class AccordionMenu extends AppCompatActivity
     }
     static void changeView(ImageButton arrow, LinearLayout hiddenView, CardView cardView)
     {
+        cardView.setOnClickListener(view ->
+        {
+            // If the CardView is already expanded, set its visibility
+            // to gone and change the arrow_right icon to arrow_down icon.
+            if (hiddenView.getVisibility() == View.VISIBLE)
+            {
+                // The transition of the hiddenView is carried out by the TransitionManager class.
+                // Here we use an object of the AutoTransition Class to create a default transition
+                TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                hiddenView.setVisibility(View.GONE);
+                arrow.setImageResource(R.drawable.outline_arrow_circle_right_24);
+            }
+
+            // If the CardView is not expanded, set its visibility to
+            // visible and change the arrow_down to arrow_right icon.
+            else
+            {
+                TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                hiddenView.setVisibility(View.VISIBLE);
+                arrow.setImageResource(R.drawable.outline_arrow_circle_down_24);
+            }
+        });
+
         arrow.setOnClickListener(view ->
         {
             // If the CardView is already expanded, set its visibility
